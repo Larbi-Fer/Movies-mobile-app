d0a236ff72d453a1322fd0a46f2f4d423d4f79b5
Larbi-Fer <ferhaoui.20044@gmail.com>
2025-10-08 16:07:44 +0100

    Initialize tailwind & nativewind AND setup navigation system

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      className="flex-1 justify-center items-center"
+    >
+      <Text className="text-dark-200 text-[25px] font-bold">Welcome to movies app</Text>
+    </View>
+  );
+}

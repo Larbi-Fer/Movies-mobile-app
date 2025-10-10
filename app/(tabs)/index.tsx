06330939eaad6933218f00aad5a8f5bfac6e947c
Larbi-Fer <ferhaoui.20044@gmail.com>
2025-10-10 15:48:04 +0100

    Implement movie listing with loading and error handling; add MovieCard component and update tailwind config

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,22 @@
+import MovieCard from "@/components/MovieCard";
 import SearchBar from "@/components/SearchBar";
+import { fetchMovies } from "@/services/api";
+import useFetch from "@/services/useFetch";
 import { Image } from "expo-image";
 import { useRouter } from "expo-router";
-import { ScrollView, View } from "react-native";
+import { ActivityIndicator, FlatList, ScrollView, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   const router = useRouter()
+
+  const {
+    data: movies,
+    loading: moviesLoading,
+    error: moviesError
+  } = useFetch(() => fetchMovies({
+    query: ''
+  }))
 
   return (
     <View className="flex-1 bg-primary">
@@ -21,6 +32,39 @@ export default function Index() {
             />
           </View>
         </SafeAreaView>
+
+        {moviesLoading ? (
+          <ActivityIndicator
+            size='large'
+            color='#0000ff'
+            className="mt10 self-center"
+          />
+        ) : moviesError ? (
+          <Text>Error: {moviesError.message}</Text>
+        ) : (
+          <View className="flex-1 mt-5">
+            <Text className="text-lg text-white font-bold mt-5 mb-3">Latest Movies</Text>
+
+            <FlatList
+              data={movies}
+              renderItem={({ item }) => (
+                <MovieCard
+                  {...item}
+                />
+              )}
+              keyExtractor={item => item.id.toString()}
+              numColumns={3}
+              columnWrapperStyle={{
+                justifyContent: 'flex-start',
+                gap: 20,
+                paddingRight: 5,
+                marginBottom: 10
+              }}
+              className="mt-2 pb-32"
+              scrollEnabled={false}
+            />
+          </View>
+        )}
       </ScrollView>
     </View>
   );

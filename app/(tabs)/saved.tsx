3890fedf33974640e610e4ce11cab77ae0cbb1dd
Larbi-Fer <ferhaoui.20044@gmail.com>
2025-10-11 18:30:52 +0100

    Customize profile and saved pages

--- app/(tabs)/saved.tsx
+++ app/(tabs)/saved.tsx
@@ -1,10 +1,14 @@
+import { SaveIcon } from 'lucide-react-native'
 import React from 'react'
 import { Text, View } from 'react-native'
 
 const Saved = () => {
   return (
-    <View>
-      <Text>Saved</Text>
+    <View className='bg-primary flex-1 px-10'>
+      <View className='flex justify-center items-center flex-1 flex-col gap-5'>
+        <SaveIcon color={'#fff'} size={30} />
+        <Text className='text-gray-500 text-base'>Save</Text>
+      </View>
     </View>
   )
 }
